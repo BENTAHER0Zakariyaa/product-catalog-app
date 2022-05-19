6972e50a7f60754af6726eaf37815a79059d382f
BENTAEHR Zakariyaa <bentaher.zakariyaa@gmail.com>
2022-05-19 16:42:49 +0100

    generate constructor/getters&setters

--- app/src/main/java/com/example/productcatalogapp/classes/User.java
+++ app/src/main/java/com/example/productcatalogapp/classes/User.java
@@ -1,4 +1,64 @@
 package com.example.productcatalogapp.classes;
 
+import java.time.LocalDateTime;
+
 public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private LocalDateTime lastLogin;
+
+    public User() {}
+
+    public User(int id, String username, String password, LocalDateTime lastLogin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.lastLogin = lastLogin;
+    }
+
+    public User(String username, String password, LocalDateTime lastLogin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.lastLogin = lastLogin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    @Override
+    public String toString() {
+        return "User (id=" + id + ", username='" + username + "', password='" + password + "', lastLogin=" + lastLogin + ")";
+    }
 }
