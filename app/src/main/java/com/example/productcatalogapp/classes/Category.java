5120704c41d9250c1943f13cd793080f883ac82c
BENTAEHR Zakariyaa <bentaher.zakariyaa@gmail.com>
2022-05-19 18:43:42 +0100

    create category class and generate getters and setters / constructor

--- app/src/main/java/com/example/productcatalogapp/classes/Category.java
+++ app/src/main/java/com/example/productcatalogapp/classes/Category.java
@@ -1,2 +1,31 @@
-package com.example.productcatalogapp.classes;public class Category {
+package com.example.productcatalogapp.classes;
+
+public class Category {
+
+    private int id;
+    private String name;
+
+    public Category(){}
+
+    public Category(int id, String name){
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
