6e0c7628a273811faebdc12f3dddc475bcfc66cc
BENTAEHR Zakariyaa <bentaher.zakariyaa@gmail.com>
2022-05-19 18:45:40 +0100

    create ProductImage class

--- app/src/main/java/com/example/productcatalogapp/classes/ProductImage.java
+++ app/src/main/java/com/example/productcatalogapp/classes/ProductImage.java
@@ -1,2 +1,30 @@
-package com.example.productcatalogapp.classes;public class ProductImage {
+package com.example.productcatalogapp.classes;
+
+public class ProductImage {
+
+    private int id;
+    private String path;
+
+    public ProductImage(){}
+
+    public ProductImage(int id, String path) {
+        this.setId(id);
+        this.setPath(path);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
