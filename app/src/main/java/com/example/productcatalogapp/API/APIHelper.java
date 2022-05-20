57819efec9bfaff4158726ab4aea1e2b94a23bb9
BENTAEHR Zakariyaa <bentaher.zakariyaa@gmail.com>
2022-05-20 17:05:33 +0100

    add password prop

---
+++ app/src/main/java/com/example/productcatalogapp/API/APIHelper.java
@@ -0,0 +1,16 @@
+package com.example.productcatalogapp.API;
+
+import com.example.productcatalogapp.classes.Category;
+
+import java.util.ArrayList;
+
+public class APIHelper {
+
+    private static final String API_URL = "";
+
+    public static ArrayList<Category> loadCategories(){
+        ArrayList<Category> categories = new ArrayList<Category>();
+
+        return categories;
+    }
+}
--- app/src/main/java/com/example/productcatalogapp/classes/User.java
+++ app/src/main/java/com/example/productcatalogapp/classes/User.java
@@ -4,31 +4,39 @@ import java.time.LocalDateTime;
 
 public class User {
 
-    private int id;
+    private Integer id;
     private String username;
+    private String password;
     private String token;
     private Boolean isConnected;
 
     public User() {}
 
-    public User(int id, String username, String token, Boolean isConnected) {
+    public User(Integer id, String username, String password,  String token, Boolean isConnected) {
         this.setId(id);
         this.setUsername(username);
+        this.setPassword(password);
         this.setToken(token);
         this.setConnected(isConnected);
+    }
+
+    public User(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
     }
 
     public User(String username, String token, Boolean isConnected) {
         this.setUsername(username);
+        this.setPassword(password);
         this.setToken(token);
         this.setConnected(isConnected);
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -38,6 +46,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getToken() {
